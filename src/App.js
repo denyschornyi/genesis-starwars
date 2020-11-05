861a97482c848f9320b6cd634c31f18004e2fa4a
Denys Chornyi <chornijj14@gmail.com>
2020-11-05 19:05:03 +0100

    Configure fetch data all planets form api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import { getAllPlanets } from "./service/getData";
 
 function App() {
+  const [allPlanets, setAllPlanets] = useState([]);
+
+  useEffect(() => {
+    getAllPlanets().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">hey</header>
     </div>
   );
 }
